72521c2d5ea99ca849e20085d6fd8c93f38faf79
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-07-08 10:07:42 +0400

    Add Angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+
+import { registerGradient } from 'devextreme-angular/common/charts';
+import { formatNumber } from 'devextreme/localization';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +9,48 @@ import { ClickEvent } from 'devextreme/ui/button';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular';
+  chartData = [
+    { month: 'January', targetSpend: 12000, actualSpend: 9500 },
+    { month: 'February', targetSpend: 15500, actualSpend: 16500 },
+    { month: 'March', targetSpend: 13500, actualSpend: 12000 },
+    { month: 'April', targetSpend: 16000, actualSpend: 14000 },
+    { month: 'May', targetSpend: 17000, actualSpend: 18500 },
+    { month: 'June', targetSpend: 20000, actualSpend: 17500 },
+    { month: 'July', targetSpend: 14500, actualSpend: 11000 },
+    { month: 'August', targetSpend: 21000, actualSpend: 22500 },
+    { month: 'September', targetSpend: 19000, actualSpend: 18000 },
+    { month: 'October', targetSpend: 22000, actualSpend: 25000 },
+    { month: 'November', targetSpend: 16000, actualSpend: 14500 },
+    { month: 'December', targetSpend: 25000, actualSpend: 27000 },
+  ];
 
-  counter = 0;
+  chartGradient = registerGradient('linear', {
+    colors: [{
+      offset: '20%',
+      color: '#dee4ff',
+    }, {
+      offset: '90%',
+      color: '#ffdeff',
+    }],
+  });
 
-  buttonText = 'Click count: 0';
+  // Type () => number (returned by calculateAverageSpend()) cannot be directly assigned to the value property of the dxi-constant-line selector.
+  averageSpend = this.calculateAverageSpend();
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  calculateAverageSpend(): number {
+    let sum = 0;
+
+    this.chartData.forEach((data) => {
+      sum += data.actualSpend;
+    });
+
+    return sum / this.chartData.length;
   }
+
+  customizeTooltip: (data: { value: number }) => { text: string } = (data: { value: number }) => {
+    const isValueAboveAverage = data.value > this.averageSpend;
+    const aboveText = `${formatNumber(data.value, 'currency')}\n${formatNumber(data.value - this.averageSpend, 'currency')} above average spending.`;
+    const belowText = `${formatNumber(data.value, 'currency')}\n${formatNumber(this.averageSpend - data.value, 'currency')} below average spending.`;
+    return { text: isValueAboveAverage ? aboveText : belowText };
+  };
 }
